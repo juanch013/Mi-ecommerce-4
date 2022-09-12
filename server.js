66a52492c7c4e7f7a2aacc11d6195619655163b0
Nicolas Caceres Sala <ncsala@gmail.com>
2022-09-12 14:24:11 -0300

    Creando controlador de getPicture y getPictures

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+
 //Swagger
 const swaggerUi = require('swagger-ui-express');
 // const YAML = require('yamljs');
@@ -7,15 +8,19 @@ const swaggerUi = require('swagger-ui-express');
 //Middleware
 const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger.yaml');
+
+// Se importa el archivo de rutas de pictures
+const picturesRoutes = require('./api/routes/picturesRoutes');
+
 //Middlewares
 const {logErrors,clientErrorHandler} = require('./api/middlewares/errorHandler');
 
 const app = express();
 const PORT = 3000;
-
 app.use(express.json());
 
-// app.use('/login', usersRoutes);
+
+app.use('/', picturesRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(logErrors);
