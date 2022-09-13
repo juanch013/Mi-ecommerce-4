24710e6a8aa42084960d3f4bf23be13e5eede2fe
Nicolas Caceres Sala <ncsala@gmail.com>
2022-09-13 14:59:07 -0300

    Funcionando el server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const route = express.Router();
 
 //const usersRoutes = require('./api/routes/usersRoutes');
 const productsRoutes = require('./api/routes/productsRoutes');
-const pictureRoutes = require('./api/routes/picturesRoutes');
+const picturesRoutes = require('./api/routes/picturesRoutes');
 const cartsRoutes = require('./api/routes/cartRoutes');
 
 //Swagger
@@ -14,20 +14,17 @@ const swaggerDocument = YAML.load('./swagger.yaml');
 //Middlewares
 const {logErrors,clientErrorHandler} = require('./api/middlewares/errorHandler');
 
-//Routes
-const productRoutes = require('./api/routes/productsRoutes')
 
 const app = express();
 const PORT = 3000; 
 
 app.use(express.json());
-//prueba
-//app.use('/api/v1/',route);
+
+//Routes
+app.use('/api/v1/',route);
 // route.use('/users', usersRoutes);
-// route.use('/products', productsRoutes);
-//route.use('/pictures', pictureRoutes);
-// route.use('/carts', cartsRoutes);
-// route.use('/pictures', pictureRoutes);
+route.use('/products', productsRoutes);
+route.use('/pictures', picturesRoutes);
 app.use('/cart', cartsRoutes);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
@@ -35,7 +32,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(logErrors);
 app.use(clientErrorHandler);
 
-app.use('/api/v1/products',productRoutes);
+
 
 
 app.listen(PORT, () => {
