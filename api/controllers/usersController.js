544f0f94e1f945c5ed5b01e73bf5c8e7be1c1060
cabrerajuanjo <juanjosecabrera@G500606MAC346.local>
2022-09-14 16:39:20 -0300

    Users finalizado

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -2,14 +2,23 @@ const bcrypt = require('bcrypt');
 const fileHelpers = require('../../helpers/filesHelpers');
 const {generateJWT} = require('../../helpers/generateJWT');
 
+
+//Validates only suported user information is accepted and they're the right data type
 const validateUserFields = (userObject) => {
-    const {email, username, password, firstname, lastname, profilepic, role} = userObject;
-    return (typeof email === 'string' && 
+    let foundInvalidField = false;
+    Object.keys(userObject).forEach((key) => {
+        const validFields = ["email", "username", "password", "firstname", "lastname", "profilepic", "role", "cart"];
+        if(!validFields.includes(key)){foundInvalidField = true;}
+    });
+    const {email, username, password, firstname, lastname, profilepic, role, cart} = userObject;
+    return (!foundInvalidField &&
+            typeof email === 'string' && 
             typeof username === 'string' && 
             typeof password === 'string' &&
             typeof firstname === 'string' && 
             typeof lastname === 'string' &&
             typeof role === 'string' &&
+            typeof cart === 'object' &&
             (typeof profilepic === 'string' || typeof profilepic === 'undefined'))
 }
 
@@ -24,51 +33,61 @@ const findUserById = (users, id) => {
         }
     })
     return userIndex;
+}
 
+// Filters out password field from user object so it's not returned
+const getUserWithoutPassword = (user) => {
+    const {id, email, username, firstname, lastname, role, profilepic, cart} = user;
+    return {id, email, username, firstname, lastname, role, profilepic, cart};
 }
 
 const usersController = {
     listUsers: function(req, res, next) {
         const users = fileHelpers.getUsers(next);
-        res.status(200).send(users);
+        const usersWithoutPassword = users.map((user) => {
+             return getUserWithoutPassword(user);
+        })
+        res.status(200).send(usersWithoutPassword);
     }, 
 
     getUser: function(req, res, next) {
         const users = fileHelpers.getUsers(next);
         const userId = Number(req.params.id);
 
+        if(isNaN(userId)) {return res.status(400).json({"msg": "Bad request: User id must be a number"});}
+
+        //Searches for userIndex. If userIndex === -1 means user wasn't found.
         const userIndex = findUserById(users, userId);
-        if(userIndex < 0)
-        {
-            return res.status(404).json({"msg": "User does not exists."})
-        }
-        return res.status(200).json(users[userIndex]);
+        if(userIndex < 0) {return res.status(404).json({"msg": "User does not exists."})}
+
+        const userWithoutPassword = getUserWithoutPassword(users[userIndex])
+        return res.status(200).json(userWithoutPassword);
 
     },
 
     createUser: function(req, res, next) {
         const userFromRequest = req.body;
-        if(!validateUserFields(userFromRequest))
-        {
-          return res.status(400).json({"msg": "Bad request"})
-        }
-        // Si usuario ya existe en la base de datos no se puede crear
         const users = fileHelpers.getUsers(next);
+
+        if(!validateUserFields(userFromRequest)) {return res.status(400).json({"msg": "Bad request"})}
+
+        // Si usuario ya existe en la base de datos no se puede crear
         const userExists = users.find((u) => u.username === userFromRequest.username);
-        if(userExists)
-        {
-            return res.status(400).json({"msg": "User already exists."});
-        }
+        if(userExists) {return res.status(400).json({"msg": "User already exists."});}
+
         // Se hashea la contraseña
         const hash = bcrypt.hashSync(userFromRequest.password, 10);
+
         // Se guarda la contrasena hasheada en el objeto
         userFromRequest.password = hash;
+
         // Se accede al id del ultimo usuario y se le suma 1
         const id = users.at(-1).id + 1;
         const userToAdd = {id, ...userFromRequest};
         users.push(userToAdd);
+
         fileHelpers.guardarUsers(users, next);
-        return res.status(201).json(userToAdd);
+        return res.status(201).json(getUserWithoutPassword(userToAdd));
     },
 
     login: async function(req, res, next) {
@@ -77,7 +96,7 @@ const usersController = {
         let users = fileHelpers.getUsers(next);
         let usuarioFind = users.find(u => u.username == username);
 
-        let passwordMatch = null
+        let passwordMatch = null;
         if (usuarioFind) {
           // Se compara la contraseña hasheada con la contraseña ingresada por el usuario
           passwordMatch = await bcrypt.compare(password, usuarioFind.password);
@@ -91,8 +110,10 @@ const usersController = {
            })
         }
 
+        const userFoundWithoutPassword = getUserWithoutPassword(usuarioFind);
+
         let payload = {
-              ...usuarioFind
+              ...userFoundWithoutPassword
            }  
      
         const token = await generateJWT(payload);
@@ -114,43 +135,44 @@ const usersController = {
         const userFromRequest = req.body;
         const userId = Number(req.params.id);
 
-        const userIndex = findUserById(users, userId);
+        if(isNaN(userId)) {return res.status(400).json({"msg": "Bad request: User id must be a number"})}
 
+        //Searches for userIndex. If userIndex === -1 means user wasn't found.
+        const userIndex = findUserById(users, userId);
         if(userIndex < 0)
         {
             return res.status(404).json({"msg": "User does not exists."})
         }
-        
-        if(!validateUserFields(userFromRequest))
-        {
-            return res.status(400).json({"msg": "Bad request"});
-        }
+        //Validates data
+        if(!validateUserFields(userFromRequest)) {return res.status(400).json({"msg": "Bad request"});}
         // Se hashea la contraseña
         userFromRequest.password = bcrypt.hashSync(userFromRequest.password, 10);
         
         users[userIndex] = {userId, ...userFromRequest};
         fileHelpers.guardarUsers(users, next);
-        return res.status(200).json(userFromRequest);
+        return res.status(200).json(getUserWithoutPassword(userFromRequest));
     },
 
     deleteUser: function(req, res,next) {
         let users = fileHelpers.getUsers(next);
         const userId = Number(req.params.id);
 
+        if(isNaN(userId)) {return res.status(400).json({"msg": "Bad request: User id must be a number"});}
+
+        //Searches for userIndex. If userIndex === -1 means user wasn't found.
         const userIndex = findUserById(users, userId);
-        console.log(userId, " ", userIndex);
-        if(userIndex < 0)
-        {
-            return res.status(404).json({"msg": "User does not exists."});
-        }
+        if(userIndex < 0) {return res.status(404).json({"msg": "User does not exists."});}
+
+        //Shifts elements back from element to delete to end and pops last element
         const userToDelete = users[userIndex];
         for(let i = userIndex; i < users.length - 1; i++)
         {
             users[i] = users[i + 1];
         }
         users.pop();
+
         fileHelpers.guardarUsers(users, next);
-        return res.status(200).json(userToDelete);
+        return res.status(200).json(getUserWithoutPassword(userToDelete));
     }
 }
 
