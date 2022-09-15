4a4e79a0c80bac7b9e32bb794e8f38dccd33b2f6
Nicolas Caceres Sala <ncsala@gmail.com>
2022-09-14 22:37:13 -0300

    Implementando libreria joi para validacion de datos

--- api/controllers/picturesController.js
+++ api/controllers/picturesController.js
@@ -7,10 +7,7 @@ const {
 
 // /pictures?product=id
 // Acción: Recupera la lista de pictures del product identificado con id. Responde con un array conteniendo las pictures.
-// Response codes:
-// 200 OK.
-// 404 Not Found (si no existe el product con el id solicitado)
-// 500 Server Error.
+// Response codes: // 200 OK.// 404 Not Found // 500 Server Error.
 // POR QUERY VIENEN COMO STRING!
 const getPictures = (req, res, next) => {
 	try {
@@ -27,17 +24,7 @@ const getPictures = (req, res, next) => {
 			});
 		}
 
-		if (!productId) {
-			return res.status(400).json({ error: 'Id is required', message: '' });
-		}
-
-		if (isNaN(productId)) {
-			return res
-				.status(400)
-				.json({ error: 'Id must be a number', message: '' });
-		}
-
-		const products = getProducts(res, next);
+		const products = getProducts(next);
 
 		const productExists = products.find(
 			(product) => product.id === parseInt(productId)
@@ -47,7 +34,7 @@ const getPictures = (req, res, next) => {
 		}
 
 		// Se lee el arhivo de pictures
-		const pictures = getImages(res, next);
+		const pictures = getImages(next);
 
 		console.log(pictures);
 		const picturesProduct = pictures?.filter(
@@ -68,10 +55,7 @@ const getPictures = (req, res, next) => {
 
 // GET /pictures/:id
 // Acción: Recupera la picture con el id solicitado. Responde con la información completa de la picture con el id buscado.
-// Response codes:
-// 200 OK.
-// 404 Not Found (si no existe la picture con el id solicitado)
-// 500 Server Error.
+// Response codes: 200 OK.// 404 Not Found // 500 Server Error.
 const getPicture = (req, res, next) => {
 	try {
 		const pictureId = req.params.id;
@@ -86,17 +70,7 @@ const getPicture = (req, res, next) => {
 			});
 		}
 
-		if (!pictureId) {
-			return res.status(400).json({ error: 'Id is required', message: '' });
-		}
-
-		if (isNaN(pictureId)) {
-			return res
-				.status(400)
-				.json({ error: 'Id must be a number', message: '' });
-		}
-
-		const pictures = getImages(res, next);
+		const pictures = getImages(next);
 
 		const picture = pictures?.find(
 			(picture) => picture.id === parseInt(pictureId)
@@ -115,10 +89,7 @@ const getPicture = (req, res, next) => {
 // POST /pictures
 // Acción: Crea una nueva picture. Debe recibir un body con la info de la picture a crear.
 // Responde con la info completa de la nueva picture.
-// Response codes:
-// 201 Created.
-// 400 Bad Request (si la llamada es incorrecta)
-// 500 Server Error.
+// Response codes: // 201 Created.//400 Bad Request // 500 Server Error.
 const createPicture = (req, res, next) => {
 	try {
 		const { pictureUrl, pictureDescription, productId } = req.body;
@@ -129,18 +100,8 @@ const createPicture = (req, res, next) => {
 			});
 		}
 
-		if (!pictureUrl || !pictureDescription || !productId) {
-			return res.status(400).json({ error: 'Missing data', message: '' });
-		}
-
-		if (isNaN(productId)) {
-			return res
-				.status(400)
-				.json({ error: 'Id must be a number', message: '' });
-		}
-
 		// Se traen los productos del archivo
-		const products = getProducts(res);
+		const products = getProducts(next);
 
 		const product = products.find(
 			(product) => product.id === parseInt(productId)
@@ -151,7 +112,7 @@ const createPicture = (req, res, next) => {
 		}
 
 		// Se traen las pictures del archivo
-		const pictures = getImages(res, next);
+		const pictures = getImages(next);
 
 		// Se calcula el id de la nueva picture
 		const pictureId = pictures.at(-1).id + 1;
@@ -177,10 +138,7 @@ const createPicture = (req, res, next) => {
 // PUT /pictures/:id
 // Acción: Actualiza la picture identificada con id. Debe recibir un body con la info de la picture a modificar.
 // Responde con la info completa de la picture modificada.
-// Response codes:
-// 200 OK.
-// 400 Bad Request (si la llamada es incorrecta)
-// 404 Not Found (si no existe la picture con el id solicitado)
+// Response codes: // 200 OK. // 400 Bad Request // 404 Not Found (si no existe la picture con el id solicitado)
 // 500 Server Error.
 const updatePicture = (req, res, next) => {
 	try {
@@ -192,18 +150,7 @@ const updatePicture = (req, res, next) => {
 				message: 'You are not authorized to access this resource',
 			});
 		}
-
-		if (!pictureUrl || !pictureDescription || !pictureId) {
-			return res.status(400).json({ error: 'Missing data', message: '' });
-		}
-
-		if (isNaN(pictureId)) {
-			return res
-				.status(400)
-				.json({ error: 'Id must be a number', message: '' });
-		}
-
-		const pictures = getImages(res, next);
+		const pictures = getImages(next);
 
 		const picture = pictures.find(
 			(picture) => picture.id === parseInt(pictureId)
@@ -228,9 +175,7 @@ const updatePicture = (req, res, next) => {
 // DELETE /pictures/:id
 // Acción: Eliminar la picture identificada con id. Responde con información sobre la eliminación realizada.
 // Response codes:
-// 200 OK.
-// 404 Not Found (si no existe la picture con el id solicitado)
-// 500 Server Error.
+// 200 OK. // 404 Not Found (si no existe la picture con el id solicitado) // 500 Server Error.
 const deletePicture = (req, res, next) => {
 	try {
 		const pictureId = req.params.id;
@@ -242,17 +187,7 @@ const deletePicture = (req, res, next) => {
 			});
 		}
 
-		if (!pictureId) {
-			return res.status(400).json({ error: 'Id is required', message: '' });
-		}
-
-		if (isNaN(pictureId)) {
-			return res
-				.status(400)
-				.json({ error: 'Id must be a number', message: '' });
-		}
-
-		const pictures = getImages(res, next);
+		const pictures = getImages(next);
 
 		const picture = pictures.find(
 			(picture) => picture.id === parseInt(pictureId)
@@ -281,74 +216,3 @@ module.exports = {
 	updatePicture,
 	deletePicture,
 };
-
-/// ######################################################################## FUNCIONES ANTERIORES
-//viejas
-// const getPicture = (req, res, next) => {
-// 	try {
-// 		const { id } = req.params;
-
-// 		if (isNaN(id)) {
-// 			return res
-// 				.status(400)
-// 				.json({ error: 'Id must be a number', message: '' });
-// 		}
-
-// 		const dbproducts = fs.readFileSync(
-// 			__dirname + '/../data/products.json',
-// 			'utf-8'
-// 		);
-// 		const products = JSON.parse(dbproducts);
-
-// 		const gallery = products.map((product) => product.gallery);
-// 		const flattenGallery = gallery.flat(1);
-// 		// Se quitan duplicados
-// 		const uniqueGallery = [...new Set(flattenGallery)];
-// 		const picture = uniqueGallery.find(
-// 			(picture) => picture['picture-id'] === parseInt(id)
-// 		);
-
-// 		if (!picture) {
-// 			return res.status(404).json({ error: 'Picture not found', message: '' });
-// 		}
-
-// 		res.status(200).json(picture);
-// 	} catch (error) {
-// 		next(error);
-// 	}
-// };
-
-// const getPictures = (req, res, next) => {
-// 	try {
-// 		const { producto } = req.query;
-
-// 		if (!producto) {
-// 			return res.status(400).json({ error: 'Id is required', message: '' });
-// 		}
-// 		console.log(typeof producto);
-
-// 		// si id no es un numero
-// 		if (isNaN(producto)) {
-// 			return res
-// 				.status(400)
-// 				.json({ error: 'Id must be a number', message: '' });
-// 		}
-
-// 		const dbproducts = fs.readFileSync(
-// 			__dirname + '/../data/products.json',
-// 			'utf-8'
-// 		);
-// 		const products = JSON.parse(dbproducts);
-// 		const product = products.find(
-// 			(product) => product.id === parseInt(producto)
-// 		);
-
-// 		if (!product) {
-// 			return res.status(404).json({ error: 'Product not found', message: '' });
-// 		}
-
-// 		res.status(200).json(product.gallery);
-// 	} catch (error) {
-// 		next(error);
-// 	}
-// };
